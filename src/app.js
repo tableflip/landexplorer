7298ffe62666f5db57dbc6614bea4093161a3818
Richard Silverton <silverton.richard@googlemail.com>
2016-10-08 08:09:44 +0100

    Adds half of the Shared Assets datasets as json

--- src/app.js
+++ src/app.js
@@ -1,38 +1,12 @@
 import React from 'react'
 import MapComponent from './map-component'
 import LayerPicker from './layer-picker'
-
-const dummyLayers = {
-  'Soil Quality': [
-    {
-      id: 0,
-      backgroundColor: 'light-red',
-      name: 'Provisional Agricultural Land Classification (ALC)',
-      provider: 'Natural England',
-      description: 'Provisional Agricultural Land Classification Grade. Agricultural land classified into five grades.'
-    },
-    {
-      id: 1,
-      backgroundColor: 'light-blue',
-      name: 'Advanced soil geochemical atlas of England and Wales',
-      provider: 'British Geological Survey',
-      description: '5700 surface soil samples, collected across England and Wales, analysed for 52 major and trace elements.'
-    }
-  ],
-  'Current Use': [
-    {
-      id: 2,
-      backgroundColor: 'light-green',
-      name: 'Land Cover Map 2007',
-      provider: 'Centre for Ecology and Hydrology',
-      description: 'Land cover describes the physical material on the surface of the country.'
-    }
-  ]
-}
+import api from './lib/api'
+import datasets from './datasets'
 
 export default class extends React.Component {
   state = {
-    layers: dummyLayers,
+    datasets: datasets,
     selectedLayers: []
   }
 
@@ -49,11 +23,17 @@ export default class extends React.Component {
     }
   }
 
+  componentDidMount () {
+    api.get({ typeName: 'topp:tasmania_state_boundaries' })
+      .then((json) => console.log('success', json))
+      .catch((err) => console.error(err))
+  }
+
   render () {
     return (
       <div className='dark-gray'>
         <div className='fl w-50'>
-          <LayerPicker layers={this.state.layers} selectedLayers={this.state.selectedLayers} toggleLayer={this.toggleLayer} />
+          <LayerPicker datasets={this.state.datasets} selectedLayers={this.state.selectedLayers} toggleLayer={this.toggleLayer} />
         </div>
         <div className='fl w-50'>
           <MapComponent />
--- src/layer-picker.js
+++ src/layer-picker.js
@@ -2,31 +2,37 @@ import React from 'react'
 
 export default class extends React.Component {
   renderLayerTile = (layer) => {
+    const backgroundColor = `hsl(${(layer.id * 209) % 360}, 60%, 75%)`
     const borderStyle = this.props.selectedLayers.indexOf(layer.id) > -1 ? 'b--black-80' : 'b--black-30'
     return (
-      <div key={layer.id} className={`w-40 mw6 mr3 ba grow pointer bg-light-gray ${borderStyle}`} onClick={this.props.toggleLayer(layer.id)}>
-        <div className={`h3 bg-${layer.backgroundColor}`}></div>
+      <div 
+        key={layer.id}
+        className={`w-40 mw6 mr3 ba grow shadow-hover pointer bg-light-gray flex-none ${borderStyle}`}
+        onClick={this.props.toggleLayer(layer.id)}
+      >
+        <div className='h3' style={{ backgroundColor }}></div>
         <div className='flex-auto pa3'>
           <div className='f3 mb3'>{layer.name}</div>
           <div className='f5 ttu tracked light-silver mb2'>{layer.provider}</div>
-          <p className='f6'>{layer.description}</p>
+          <p className='f6 h4 overflow-hidden'>{layer.description}</p>
         </div>
       </div>
     )
   }
 
   renderCategory = (category) => {
-    return (<div key={category} className='pb3'>
-      <h1 className='f2 mb3'>{category}</h1>
-      <div className='flex items-stretch justify-start'>
-        {this.props.layers[category].map(this.renderLayerTile)}
+    return (<div key={category.name} className='pb3'>
+      <h1 className='f2 mb2'>{category.category}</h1>
+      <h2 className='f4 mb3 silver'>{category.description}</h2>
+      <div className='flex items-stretch justify-start w-100 overflow-x-auto overflow-y-hidden pa3'>
+        {category.datasets.map(this.renderLayerTile)}
       </div>
     </div>)
   }
 
   render () {
-    return (<div className='pa3'>
-      {Object.keys(this.props.layers).map(this.renderCategory)}
+    return (<div className='pa3 overflow-y-auto' style={{ height: '100vh' }}>
+      {this.props.datasets.map(this.renderCategory)}
     </div>)
   }
 }
