ce9dcd8542e3870711207cfcf62085dce1eaf0f6
Bernard Mordan <bernardmordan@gmail.com>
2016-11-24 11:16:20 +0000

    yep more styling

--- src/pages/place/place-intro.js
+++ src/pages/place/place-intro.js
@@ -8,12 +8,12 @@ export default class extends React.Component {
     const wikiEntry = this.props.wikiEntry
     const { latitude, longitude } = this.props.viewport
     return (
-      <section className='bg-white'>
-        <div className='pa4' style={{minHeight: '4rem'}}>
+      <section className='mh3 bg-white bb b--black-30 pb3'>
+        <div className='pv2' style={{minHeight: '4rem'}}>
           <label className='f6 black-40'>Title of address</label>
-          <div className='f4 mt1 black-40'>{address} {postcode && `, ${postcode}`}</div>
+          <div className='f4 mt1 black-40'>{address || 'Unknown location'} {postcode && `, ${postcode}`}</div>
         </div>
-        <div className='dt dt--fixed w-100 pa4 f6'>
+        <div className='dt dt--fixed w-100 pv2 f6'>
           <div className='dtc'>
             <label className='black-40'>Location</label>
             <div className='pt1'>{round(longitude, 3)}, {round(latitude, 3)}</div>
@@ -33,7 +33,11 @@ export default class extends React.Component {
             </span>
           </div>
         </div>
-        <div className='pa4'>
+        <div className='pv2'>
+          <label className='f6 black-40'>Land owner</label>
+          <div className='mt1'>According to <a href='https://eservices.landregistry.gov.uk/www/wps/portal/!ut/p/b1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOKNjSxMDA1NjDwsjM3MDTxN3dyNDUNMjQ1MjIEKIvEocDckpD84NU8_XD8KTZm_m6OBp7GTv7e7caCRgYEZhgJUcyyMCCgAuQOswAAHcDTQ9_PIz03VL8iNqPDMDEgHAAblRA0!/dl4/d5/L0lDU0lKSmdrS0NsRUpDZ3BSQ2dwUkNTQS9ZSVVJQUFJSUlJTU1JS0VFQUFDR09HT0NHSUJKRkpGQkpORE5EQk5ISUVBTExBISEvNEczYUQyZ2p2eWhDa3lGTU5RaWt5RktOUmprS2NhZ21Rb2dnL1o3XzMyODQxMTQySDgzNjcwSTVGRzMxVDUzOFY0LzAvaWJtLmludi8zNDEyMzMwNTA1MDcvc3BmX0FjdGlvbk5hbWUvc3BmX0FjdGlvbkxpc3RlbmVyL3NwZl9zdHJ1dHNBY3Rpb24vITJmTHJJbnNwaXJlSWRJbml0LmRv/' target='_Blank'>Land Registry</a> the owner ID for this location is ID 2934692389</div>
+        </div>
+        <div className='pv2'>
           <div className='dib w-70'>
             <label className='f6 black-40'>About this land</label>
             <div style={{minHeight: '4rem'}}>
@@ -41,12 +45,20 @@ export default class extends React.Component {
             </div>
           </div>
         </div>
-        <div className='dt dt--fixed w-100 pa4'>
+        <div className='dt dt--fixed w-100 pv2'>
           <div className='dtc'>
-            <label className='f6 black-40'>Land use</label>
+            <label className='db f6 black-40'>Land use</label>
+            <div className='dt dt--fixed w-100 pt2'>
+              <label className='dtc pr1'>Trunk</label>
+              <label className='dtc pr1'>Crop</label>
+            </div>
           </div>
           <div className='dtc'>
-            <label className='f6 black-40'>Land cover</label>
+            <label className='db f6 black-40'>Land cover</label>
+            <div className='dt dt--fixed w-100 pt2'>
+              <label className='dtc pr1'>Grass</label>
+              <label className='dtc pr1'>Wood</label>
+            </div>
           </div>
         </div>
       </section>
