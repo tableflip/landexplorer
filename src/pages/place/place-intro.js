99f3a9815bb7500d7d659757bf92363bf77d9c73
Oli Evans <oli@zilla.org.uk>
2017-01-02 21:31:25 +0000

    Gross hack to deal with landregistry website

--- src/pages/place/place-intro.js
+++ src/pages/place/place-intro.js
@@ -7,10 +7,27 @@ import Icon from './icon'
 import Share from './share'
 
 export default ({placeData, wikiEntry, lngLat, features, location, data}) => {
-  console.log('data', data)
   const { address, postcode, place } = placeData
   const { landcover, landuse, landuse_overlay, contour } = filterFeatures(features)
   const uniqueLandFeatures = uniq(landuse.concat(landuse_overlay).concat(landcover))
+
+  // Sorry. To allow the user to click through to the results the land registry
+  // search, we have to ensure that their browser just visited the page on the
+  // eservices.landregistry.gov.uk that shows the INSPIRE form.
+  // I do admit that I take gross pleasure in the fact that hideous dance works.
+  const LAND_REGISTRY_WINDOW = 'landregistry'
+  const preWarmLandRegistry = (e) => {
+    window.open(
+      'https://eservices.landregistry.gov.uk/www/wps/portal/!ut/p/b1/hc7NCoJAFAXgR7q_jrO1heMQJGFQziZmIWHouAmf32xXUd7dge8cLgRojZBVZsrhAiHFub_FRz-lOKw5mKuwVSLlyorJ0WelEzplgipP0P4Bjrb6TZfgDOGD1WWBXnb13smREc0XeN-xvAHWP14Af1yBcKimsYMxDLbxd10AehjogQ!!/dl4/d5/L0lDU0lKSmdrS0NsRUpDZ3BSQ2dwUkNTQS9ZSVVJQUFJSUlJTU1JS0VFQUFDR09HT0NHSUJKRkpGQkpORE5EQk5ISUVBTExBISEvNEczYUQyZ2p2eWhDa3lGTU5RaWt5RktOUmprS2NhZ21Rb2dnL1o3XzMyODQxMTQySDgzNjcwSTVGRzMxVDUzOFY0LzAvaWJtLmludi8zNDQ2Mzc4Mjc2MTYvc3BmX0FjdGlvbk5hbWUvc3BmX0FjdGlvbkxpc3RlbmVyL3NwZl9zdHJ1dHNBY3Rpb24vITJmTHJJbnNwaXJlSWRJbml0LmRv/',
+      LAND_REGISTRY_WINDOW
+    )
+    e.preventDefault()
+    const form = e.target
+    setTimeout(() => {
+      form.submit()
+    }, 100)
+  }
+
   return (
     <section className='pb3 ph3 bb b--black-20'>
       <div className='pv2' style={{minHeight: '4rem'}}>
@@ -45,11 +62,11 @@ export default ({placeData, wikiEntry, lngLat, features, location, data}) => {
         <div className='mt1 pt1'>
           {data.loading && <ThreeBounce color='lightgray' size={12} />}
           {data.inspireId && (
-            <form target='_blank' method='post' action='https://eservices.landregistry.gov.uk/www/wps/portal/!ut/p/b1/hc7bCoJAEAbgZ_EBYsbd1bZLKzyQJqaU7o0ImWgeosIOT592V2HO3cD3_zMgIFIYMk5VOoMQRJ20eZZc86ZOyn4XakwJZ7LMiNmhKVqKblA5UCgy2oHoDzDksfwOQmSxX_CT87iG9nPRBsXTI85y97hovl9mopqcKzdvGm112Ld3T5LAT-suJ76qXV1Di87dlUE9gqj-gM_bnIyA_vc3wIHRENZmU6UQdWw61MO3DCpR2uy4MW-ZJL0A0_XqsQ!!/dl4/d5/L0lDU0lKSmdwcGlRb0tVUW9LVVEhL29Gb2dBRUlRaGpFQ1VJZ0FJQUl5RkFNaHdVaFM0SldsYTRvIS80RzNhRDJnanZ5aERVd3BNaFFqVW81Q2pHcHhBL1o3XzMyODQxMTQySDgzNjcwSTVGRzMxVDUzOFY0LzAvMzQ0NjE5NjQzNDkxL3NwZl9BY3Rpb25OYW1lL3NwZl9BY3Rpb25MaXN0ZW5lci9zcGZfc3RydXRzQWN0aW9uLyEyZlFEU2VhcmNoLmRv/'>
+            <form onSubmit={preWarmLandRegistry} target={LAND_REGISTRY_WINDOW} method='post' action='https://eservices.landregistry.gov.uk/www/wps/portal/!ut/p/b1/hc7bCoJAEAbgZ_EBYsbd1bZLKzyQJqaU7o0ImWgeosIOT592V2HO3cD3_zMgIFIYMk5VOoMQRJ20eZZc86ZOyn4XakwJZ7LMiNmhKVqKblA5UCgy2oHoDzDksfwOQmSxX_CT87iG9nPRBsXTI85y97hovl9mopqcKzdvGm112Ld3T5LAT-suJ76qXV1Di87dlUE9gqj-gM_bnIyA_vc3wIHRENZmU6UQdWw61MO3DCpR2uy4MW-ZJL0A0_XqsQ!!/dl4/d5/L0lDU0lKSmdwcGlRb0tVUW9LVVEhL29Gb2dBRUlRaGpFQ1VJZ0FJQUl5RkFNaHdVaFM0SldsYTRvIS80RzNhRDJnanZ5aERVd3BNaFFqVW81Q2pHcHhBL1o3XzMyODQxMTQySDgzNjcwSTVGRzMxVDUzOFY0LzAvMzQ0NjE5NjQzNDkxL3NwZl9BY3Rpb25OYW1lL3NwZl9BY3Rpb25MaXN0ZW5lci9zcGZfc3RydXRzQWN0aW9uLyEyZlFEU2VhcmNoLmRv/'>
               <input type='hidden' name='polygonId' value={data.inspireId} />
               <button title='Click to look it up on landregistry.gov.uk. Opens in a new window.' className='pl0' style={{cursor: 'pointer', border: 'none', background: 'transparent'}}>
                 <span className='black-60 f5 underline'>{data.inspireId}</span>
-                <small className='f6 black-40'> 🔗 Find it on landregistry.gov.uk </small>
+                <small className='f6 black-40'> 🔗 Find it on landregistry.gov.uk</small>
               </button>
             </form>
           )}
