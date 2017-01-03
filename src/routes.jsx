7b5391f9b4046034a452dd6a7a05a016282b97cd
Oli Evans <oli@zilla.org.uk>
2017-01-03 13:13:08 +0000

    Add GA

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Router, Route, IndexRoute, applyRouterMiddleware } from 'react-router'
 import { useScroll } from 'react-router-scroll'
+import ReactGA from 'react-ga'
 import Layout from './layout.jsx'
 import Place from './pages/place'
 import Data from './pages/data'
+import config from './config'
+
+ReactGA.initialize(config.googleTrackingId)
+
+function logPageView () {
+  ReactGA.set({ page: window.location.pathname })
+  ReactGA.pageview(window.location.pathname)
+}
 
 const Routes = (props) => {
   const { history } = props
   return (
-    <Router history={history} render={applyRouterMiddleware(useScroll())}>
+    <Router history={history} render={applyRouterMiddleware(useScroll())} onUpdate={logPageView}>
       <Route path='/' component={Layout}>
         <IndexRoute component={Place} />
         <Route path='/:category' component={Data} />
